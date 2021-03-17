e056f924d774ef4b90bf5b404c1304d7c794b88b
Timur Radko <timurradko@gmail.com>
2021-03-17 13:04:09 +0300

    added implementation of REST API data server

---
+++ src/main/java/com/runar/app/springboot/dataserver/runarappdataserver/PictureRestController.java
@@ -0,0 +1,66 @@
+package com.runar.app.springboot.dataserver.runarappdataserver;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class PictureRestController {
+    private static final String STORAGE_DIRECTORY_PATH = "storage/";
+    private static final String CONTENT_TYPE = "multipart/form-data";
+
+    @PostMapping("/upload")
+    public ResponseEntity uploadToLocalFileSystem(@RequestParam("file") MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path path = Paths.get(STORAGE_DIRECTORY_PATH + fileName);
+        try {
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/download/")
+                .path(fileName)
+                .toUriString();
+        return ResponseEntity.ok(fileDownloadUri);
+    }
+
+    @PostMapping("/multi-upload")
+    public ResponseEntity multiUpload(@RequestParam("files") MultipartFile[] files) {
+        List<Object> fileDownloadUrls = new ArrayList<>();
+        Arrays.stream(files)
+                .forEach(file -> fileDownloadUrls.add(uploadToLocalFileSystem(file).getBody()));
+        return ResponseEntity.ok(fileDownloadUrls);
+    }
+
+    @GetMapping("/download/{fileName:.+}")
+    public ResponseEntity downloadFileFromLocal(@PathVariable String fileName) {
+        Path path = Paths.get(STORAGE_DIRECTORY_PATH + fileName);
+        Resource resource = null;
+        try {
+            resource = new UrlResource(path.toUri());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(CONTENT_TYPE))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+}
---
+++ src/main/java/com/runar/app/springboot/dataserver/runarappdataserver/RunarAppDataServerApplication.java
@@ -0,0 +1,13 @@
+package com.runar.app.springboot.dataserver.runarappdataserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunarAppDataServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunarAppDataServerApplication.class, args);
+    }
+
+}
